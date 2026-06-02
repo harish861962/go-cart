32a9960fc5813c797ce92016cb0c7b8346d22e72
harish861962 <harishsaini1266@gmail.com>
2026-06-02 18:59:08 +0530

    clean code final

--- src/App.jsx
+++ src/App.jsx
@@ -9,6 +9,37 @@
 // import Navbar from '../components/Navbar';
 
 
+// import { useState } from 'react'
+// import Hero from '@components/Hero'
+// import Productsheader from '@components/productsheader';
+// import Services from '@components/Services';
+// import Newsletter from '@components/NewsLetter';
+// import Footer from '@components/Footer';
+// import LatestProducts from '@components/latestProducts';
+// import BestSellingProducts from '@components/bestSellingProducts';
+// import Navbar from '@components/Navbar';
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <Navbar/>
+//       <Hero/>
+//       <LatestProducts/>
+//     <BestSellingProducts/>
+//       <Services/>
+//       <Newsletter/>
+//       <Footer/>
+  
+//     </>
+//   )
+// }
+
+// export default App
+
+
 import { useState } from 'react'
 import Hero from '@components/Hero'
 import Productsheader from '@components/productsheader';
@@ -19,20 +50,16 @@ import LatestProducts from '@components/latestProducts';
 import BestSellingProducts from '@components/bestSellingProducts';
 import Navbar from '@components/Navbar';
 
-
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
       <Navbar/>
       <Hero/>
       <LatestProducts/>
-    <BestSellingProducts/>
+      <BestSellingProducts/>
       <Services/>
       <Newsletter/>
       <Footer/>
-  
     </>
   )
 }
