1d8e00e1715269786cbce2b3a44b18d488162136
harish861962 <harishsaini1266@gmail.com>
2026-06-02 18:10:32 +0530

    fix import paths

--- src/main.jsx
+++ src/main.jsx
@@ -5,15 +5,24 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import 'bootstrap-icons/font/bootstrap-icons.css';
 import App from './App.jsx'
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+
+// ✅ CORRECT PATHS
 import Navbar from './components/Navbar.jsx';
 import Productsdetails from './components/ProductsDetails.jsx';
-import { ToastContainer, toast } from 'react-toastify';
-import 'react-toastify/dist/ReactToastify.css';
 import Shop from './pages/shop.jsx';
 import Cart from './pages/cart.jsx';
-import { CartProvider } from "react-use-cart";
 import Login from './pages/login.jsx';
 import Signup from './pages/signup.jsx';
+
+import { ToastContainer, toast } from 'react-toastify';
+import 'react-toastify/dist/ReactToastify.css';
+import { CartProvider } from "react-use-cart";
+// import Navbar from './components/Navbar.jsx';
+// import Productsdetails from './components/ProductsDetails.jsx';
+// import Shop from './pages/shop.jsx';
+// import Cart from './pages/cart.jsx';
+// import Login from './pages/login.jsx';
+// import Signup from './pages/signup.jsx';
 
 createRoot(document.getElementById('root')).render(
   <BrowserRouter>
