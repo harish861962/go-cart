2d8a4f72b5d9972936cd9a8907f5755feb8831fd
harish861962 <harishsaini1266@gmail.com>
2026-06-02 18:18:33 +0530

    fix main.jsx import paths

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
 import { useState } from 'react'
-import Hero from './components/Hero'
-import Productsheader from './components/productsheader';
-import Productsdetails from './components/ProductsDetails';
-import Services from './components/Services';
-import Newsletter from './components/NewsLetter';
-import Footer from './components/Footer';
-import Login from './pages/login';
-import LatestProducts from './components/latestProducts';
-import BestSellingProducts from './components/bestSellingProducts';
-import Navbar from './components/Navbar';
+import Hero from '../components/Hero'
+import Productsheader from '../components/productsheader';
+import Services from '../components/Services';
+import Newsletter from '../components/NewsLetter';
+import Footer from '../components/Footer';
+import LatestProducts from '../components/latestProducts';
+import BestSellingProducts from '../components/bestSellingProducts';
+import Navbar from '../components/Navbar';
 
 
 function App() {
